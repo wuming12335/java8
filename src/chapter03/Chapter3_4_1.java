4c0eb56c6f9e856fd308d7d30181785964ee99ad
wuming12335 <499707547@qq.com>
2018-12-24 22:24:27 +0800

    使用Predicate

--- src/chapter03/Chapter3_4_1.java
+++ src/chapter03/Chapter3_4_1.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//该注解表示改接口会被设计成为一个函数式接口
+/**
+ * 该注解表示改接口会被设计成为一个函数式接口
+ */
 @FunctionalInterface
 interface Predicate<T> {
 	boolean test(T t);
 }
+
 public class Chapter3_4_1 {
 	public static <T> List<T> filter(List<T> list, Predicate<T> p) {
 		List<T> results = new ArrayList<>();
